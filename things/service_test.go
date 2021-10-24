f5bf23321aa2a574e18ec7d61c292e29303df1b4
Anand Sivaram Palassery <aspnair@gmail.com>
2021-10-24 15:18:47 +0530

    MF-1425 - Test cases changes for SDK
    MF-1425 Enhancement to supply an external UUID for Things and Channels.
    These are the new testcases added for
    - Things Service Testcases
    - SDK Things and Channel Testcases
    
    Signed-off-by: Anand Sivaram Palassery <aspnair@gmail.com>
    Signed-off-by: Q.s <wangqs_eclipse@yahoo.com>

--- things/service_test.go
+++ things/service_test.go
@@ -50,6 +50,8 @@ func newService(tokens map[string]string) things.Service {
 
 func TestCreateThings(t *testing.T) {
 	svc := newService(map[string]string{token: email})
+	meta := things.Metadata{}
+	meta["type"] = "extid"
 
 	cases := []struct {
 		desc   string
@@ -68,6 +70,20 @@ func TestCreateThings(t *testing.T) {
 			things: []things.Thing{{Name: "e"}},
 			token:  wrongValue,
 			err:    things.ErrUnauthorizedAccess,
+		},
+		{
+			desc:   "create new things with external UUID",
+			things: []things.Thing{{ID: "fe6b4e92-cc98-425e-b0aa-000000000001", Name: "a", Metadata: meta},
+				{ID: "fe6b4e92-cc98-425e-b0aa-000000000002", Name: "b", Metadata: meta}},
+			token:  token,
+			err:    nil,
+		},
+		{
+			desc:   "create new things with external wrong UUID",
+			things: []things.Thing{{ID: "b0aa-000000000001", Name: "a", Metadata: meta},
+				{ID: "b0aa-000000000002", Name: "b", Metadata: meta}},
+			token:  token,
+			err:    nil,
 		},
 	}
 
@@ -599,6 +615,8 @@ func TestRemoveThing(t *testing.T) {
 
 func TestCreateChannels(t *testing.T) {
 	svc := newService(map[string]string{token: email})
+	meta := things.Metadata{}
+	meta["type"] = "extid"
 
 	cases := []struct {
 		desc     string
@@ -617,6 +635,20 @@ func TestCreateChannels(t *testing.T) {
 			channels: []things.Channel{{Name: "e"}},
 			token:    wrongValue,
 			err:      things.ErrUnauthorizedAccess,
+		},
+		{
+			desc:   "create new channels with external UUID",
+			channels: []things.Channel{{ID: "fe6b4e92-cc98-425e-b0aa-000000000001", Name: "a", Metadata: meta},
+				{ID: "fe6b4e92-cc98-425e-b0aa-000000000002", Name: "b", Metadata: meta}},
+			token:  token,
+			err:    nil,
+		},
+		{
+			desc:   "create new channels with external wrong UUID",
+			channels: []things.Channel{{ID: "b0aa-000000000001", Name: "a", Metadata: meta},
+				{ID: "b0aa-000000000002", Name: "b", Metadata: meta}},
+			token:  token,
+			err:    nil,
 		},
 	}
 
